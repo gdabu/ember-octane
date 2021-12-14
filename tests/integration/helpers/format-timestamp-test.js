63bf3145b5cb3267beffb58539056e2b69a338b4
geoff <geoffdabu@gmail.com>
2021-12-14 12:05:33 -0800

    Helper Integration Testing

--- tests/integration/helpers/format-timestamp-test.js
+++ tests/integration/helpers/format-timestamp-test.js
@@ -3,15 +3,15 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Helper | format-timestamp', function(hooks) {
+module('Integration | Helper | format-timestamp', function (hooks) {
   setupRenderingTest(hooks);
 
   // Replace this with your real tests.
-  test('it renders', async function(assert) {
-    this.set('inputValue', '1234');
+  test('it renders', async function (assert) {
+    this.set('myDate', '04-05-1983');
 
-    await render(hbs`{{format-timestamp inputValue}}`);
+    await render(hbs`{{format-timestamp myDate}}`);
 
-    assert.equal(this.element.textContent.trim(), '1234');
+    assert.equal(this.element.textContent.trim(), 'Apr 5, 1983 00:00.00 AM');
   });
 });
