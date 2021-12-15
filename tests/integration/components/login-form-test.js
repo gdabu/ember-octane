ecf128501e28a90df46e6db94c311f5eb089dc44
geoff <geoffdabu@gmail.com>
2021-12-15 10:36:10 -0800

    Component Integration Tests

--- tests/integration/components/login-form-test.js
+++ tests/integration/components/login-form-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, fillIn, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | login-form', function (hooks) {
@@ -17,13 +17,14 @@ module('Integration | Component | login-form', function (hooks) {
         .trim()
         .replace(/\s*\n+\s*/g, '\n')
         .split('\n'),
-      [
-        'Login',
-        'Select a user',
-        'Testy Testerson',
-        'Sample McData',
-        'A validation message',
-      ]
+      ['Login', 'Select a user', 'Testy Testerson', 'Sample McData']
     );
+
+    let button = /**@type {HTMLInputElement}*/ find('input[type=submit]');
+    assert.equal(button.disabled, true);
+    // button is disabled
+    await fillIn('select', '1');
+    // button is enabled
+    assert.equal(button.disabled, false);
   });
 });
