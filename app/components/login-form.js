2a037279431f8b08304c3c4165d9f007d6296317
geoff <geoffdabu@gmail.com>
2021-12-14 19:38:04 -0800

    using tracked properties

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,8 +1,14 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class LoginFormComponent extends Component {
+  @tracked
   userId = 1;
+
+  get isDisabled() {
+    return !this.userId;
+  }
 
   loginAsUserWithId(val) {
     console.log(val);
@@ -18,5 +24,14 @@ export default class LoginFormComponent extends Component {
     const val = target.querySelector('select').value;
     evt.preventDefault();
     this.loginAsUserWithId(val);
+  }
+
+  /**
+   *
+   * @param {Event & {target: HTMLSelectElement}} evt
+   */
+  @action
+  onSelectChanged(evt) {
+    this.userId = evt.target.value;
   }
 }
