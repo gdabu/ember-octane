4e56a70886908104812777d0e10163a95d434d09
geoff <geoffdabu@gmail.com>
2021-12-14 19:22:13 -0800

    Stateful Components

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -2,6 +2,8 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
 export default class LoginFormComponent extends Component {
+  userId = 1;
+
   loginAsUserWithId(val) {
     console.log(val);
   }
