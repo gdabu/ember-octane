1cb3cbee310fce1c31f9a1915cdf2911815329b7
geoff <geoffdabu@gmail.com>
2021-12-16 12:52:52 -0800

    Guarded login & team routes

--- app/routes/teams.js
+++ app/routes/teams.js
@@ -1,4 +1,17 @@
 import Route from '@ember/routing/route';
-
+import { inject as service } from '@ember/service';
+import AuthService from 'shlack/services/auth';
 export default class TeamsRoute extends Route {
+  /**
+   *
+   * @param {*} AuthService
+   */
+  @service auth;
+  async beforeModel(transition) {
+    await super.beforeModel(transition);
+
+    if (!this.auth.currentUserId) {
+      this.transitionTo('login');
+    }
+  }
 }
